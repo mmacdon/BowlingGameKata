d1a71b7d469ea63b2bd49e418e0dd220806f6c1a
Mary Macdonald <mary.macdonald@mims.com>
2022-04-19 14:48:56 +1000

    initial

--- BowlingGameKata/BowlingGameTest.cs
+++ BowlingGameKata/BowlingGameTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
+﻿using Xunit;
 
 namespace BowlingGameKata
 {
@@ -14,20 +9,22 @@ namespace BowlingGameKata
         [Fact]
         public void TestGutterGame()
         {
-            for(int i = 0; i < 10; i++)
-            {
-                _game.Roll(0);
-            }
+            RollMany(20, 0);
             Assert.Equal(0, _game.Score());
+        }
+
+        private void RollMany(int n, int pins)
+        {
+            for(int i = 0;i < n; i++)
+            {
+                _game.Roll(pins);
+            }
         }
 
         [Fact]
         public void TestAllOnes()
         {
-            for (int i = 0; i < 20; i++)
-            {
-                _game.Roll(1);
-            }
+            RollMany(20, 1);
             Assert.Equal(20, _game.Score());
         }
     }
