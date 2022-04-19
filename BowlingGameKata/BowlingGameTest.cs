2130bccdaf0445ae3469f5ebe587a25a4b4909cb
Mary Macdonald <mary.macdonald@mims.com>
2022-04-19 14:45:05 +1000

    Add project files.

---
+++ BowlingGameKata/BowlingGameTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BowlingGameKata
+{
+    public class BowlingGameTest
+    {
+
+        private readonly Game _game = new Game();
+        [Fact]
+        public void TestGutterGame()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                _game.Roll(0);
+            }
+            Assert.Equal(0, _game.Score());
+        }
+
+        [Fact]
+        public void TestAllOnes()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                _game.Roll(1);
+            }
+            Assert.Equal(20, _game.Score());
+        }
+    }
+}
---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,15 @@
+﻿namespace BowlingGameKata;
+public class Game
+{
+    private int _score = 0;
+
+    public void Roll(int pins)
+    {
+        _score += pins;
+    }
+
+    public int Score()
+    {
+        return _score;
+    }
+}
